a58ca759f0615c40be5986eef94010e1142c8a8c
Ole Odendahl <oleodendahl@gmail.com>
2021-07-03 17:22:06 +0200

    Added middleware for graphene.

---
+++ firebase_auth/middleware.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import authenticate
+
+AUTH_HEADER_NAME = "HTTP_AUTHORIZATION"
+
+
+class FirebaseAuthGrapheneMiddleware():
+    """Middleware to be used with graphene to authenticate the firebase user via token."""
+
+    def resolve(self, next_chain, root, info, **kwargs):
+        context = info.context
+        token = self.get_token(context)
+
+        if token is not None:
+            user = authenticate(request=context, **kwargs)
+
+            if user is not None:
+                context.user = user
+
+        return next_chain(root, info, **kwargs)
+
+    def get_token(self, context):
+        auth_data = context.META.get(AUTH_HEADER_NAME, '').split()
+
+        if len(auth_data) == 1 and auth_data[0] != '':
+            return auth_data[0]
+        elif len(auth_data) == 2:
+            return auth_data[1]
+
+        return None
